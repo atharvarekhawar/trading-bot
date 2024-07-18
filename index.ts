7eaaf2c4f8c42d9397769bd2f0499f09b288a472
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2024-07-18 16:55:14 +0530

    completed create order function

--- index.ts
+++ index.ts
@@ -1,29 +1,44 @@
 import axios from "axios"
 import { DepthManager } from "./DepthManager"
+import { cancelAll, createOrder } from "./order";
 
-const solInrMarket = new DepthManager("B-XAI_INR");
+const xaiInrMarket = new DepthManager("B-XAI_INR");
 
-const solUsdtMarket = new DepthManager("B-XAI_USDT");
+const xaiUsdtMarket = new DepthManager("B-XAI_USDT");
 
 const usdtInrMarket = new DepthManager("B-USDT_INR");
 
 setInterval(() => {
-    console.log(solInrMarket.getRelevantDepth());
-    console.log(solUsdtMarket.getRelevantDepth());
+    console.log(xaiInrMarket.getRelevantDepth());
+    console.log(xaiUsdtMarket.getRelevantDepth());
     console.log(usdtInrMarket.getRelevantDepth());
 
     // first way is to sell first and then buy
-    const canGetInr = solInrMarket.getRelevantDepth().lowestAsk - 0.001;
+    const canGetInr = xaiInrMarket.getRelevantDepth().lowestAsk - 0.001;
     const canGetUsdt = canGetInr /usdtInrMarket.getRelevantDepth().lowestAsk;
-    const canGetSol = canGetUsdt / solUsdtMarket.getRelevantDepth().lowestAsk;
+    const canGetSol = canGetUsdt / xaiUsdtMarket.getRelevantDepth().lowestAsk;
 
     console.log(`we convert 1 XAI to ${canGetSol}`);
 
     // second way is to buy first and then sell
-    const initialInr = solInrMarket.getRelevantDepth().highestBid + 0.001;
-    const canGetUsdt2 = solUsdtMarket.getRelevantDepth().highestBid;
+    const initialInr = xaiInrMarket.getRelevantDepth().highestBid + 0.001;
+    const canGetUsdt2 = xaiUsdtMarket.getRelevantDepth().highestBid;
     const canGetInr2 = canGetUsdt2 * usdtInrMarket.getRelevantDepth().highestBid;
 
     console.log(`we convert ${initialInr} to ${canGetInr2}`);
     
 },2000)
+
+ const main = async () => {
+    const highestBid = xaiInrMarket.getRelevantDepth().highestBid;
+    console.log(`placing order for ${parseFloat(highestBid) + 0.01}`);
+    await createOrder("buy", "XAIINR", (parseFloat(highestBid) + 0.01).toFixed(3), 10, Math.random().toString())
+    await new Promise((r) => setTimeout(r, 10000));
+    await cancelAll("XAIINR");
+    await new Promise((r) => setTimeout(r, 1000));
+    main();
+}
+
+setTimeout(async () => {
+    main();
+}, 2000)
